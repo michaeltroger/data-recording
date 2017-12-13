91ad9ec61ecfc3ef9a5e34561a9977ea3043b87b
Michael Troger <dev@michaeltroger.com>
2017-12-13 21:16:41 +0100

    Write values into seperate files

--- mobile/src/main/java/com/michaeltroger/datarecording/sensor/PersistDataTask.java
+++ mobile/src/main/java/com/michaeltroger/datarecording/sensor/PersistDataTask.java
@@ -6,50 +6,81 @@ import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
-import com.opencsv.CSVWriter;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 // TODO: do the actual implementation
 public class PersistDataTask extends AsyncTask<Void, Void, Void> {
 
     private static final String APP_DIRECTORY = "DataRecording";
-    private static final String TAG =PersistDataTask.class.getSimpleName();
+    private static final String FILE_EXTENSION = ".csv";
+    private static final String TAG = PersistDataTask.class.getSimpleName();
+    private static final String COMMA_DELIMITER = ",";
+    private static final String NEW_LINE_SEPARATOR = "\n";
+
+    private List<FileWriter> fileWriters;
 
     private ConcurrentLinkedQueue<float[]> valuesQueue = new ConcurrentLinkedQueue<>();
-    private final FileWriter fileWriter;
 
     public PersistDataTask() throws IOException {
-        final String baseDir = Environment.getExternalStorageDirectory().getAbsolutePath();
-        final String fileName = "test.csv"; // TODO: dynamically choose file
-        final String filePath = baseDir + File.separator + APP_DIRECTORY + File.separator + fileName;
+        createFileWriters();
+        writeStaticSampleInfosIntoFile();
+    }
 
+    private void createFileWriters() throws IOException {
         final File folder = new File( Environment.getExternalStorageDirectory(), APP_DIRECTORY);
         if (!folder.exists()) {
             folder.mkdir();
         }
 
-        final File file = new File(filePath);
-        if (file.exists()) {
-            fileWriter = new FileWriter(filePath, true);
-        } else {
-            fileWriter = new FileWriter(filePath);
-        }
+        final String baseDir = Environment.getExternalStorageDirectory().getAbsolutePath();
 
+        List<String> sensors = new ArrayList<>();
+        sensors.add("accX");
+        sensors.add("accY");
+
+        fileWriters = new ArrayList<>();
+
+        for (final String sensor : sensors) {
+            final String fileName = sensor+FILE_EXTENSION;
+            final String filePath = baseDir + File.separator + APP_DIRECTORY + File.separator + fileName;
+
+            final File file = new File(filePath);
+            final FileWriter fileWriter;
+            if (file.exists()) {
+                fileWriter = new FileWriter(filePath, true);
+            } else {
+                fileWriter = new FileWriter(filePath);
+            }
+
+            fileWriters.add(fileWriter);
+
+        }
+        // TODO: dynamically choose file
+
+    }
+
+    private void writeStaticSampleInfosIntoFile() {
+        for (final FileWriter fileWriter : fileWriters) {
+            try {
+                // TODO: get data from user input
+                fileWriter.append("room");
+                fileWriter.append(COMMA_DELIMITER);
+                fileWriter.append("martina");
+                fileWriter.append(COMMA_DELIMITER);
+                fileWriter.append("hagenberg");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     protected Void doInBackground(Void... voids) {
-         final CSVWriter csvWriter = new CSVWriter(
-                fileWriter,
-                CSVWriter.DEFAULT_SEPARATOR,
-                CSVWriter.NO_QUOTE_CHARACTER,
-                CSVWriter.DEFAULT_ESCAPE_CHARACTER,
-                CSVWriter.DEFAULT_LINE_END);
-
         while (true) {
             if (isCancelled()) {
                 return null;
@@ -60,19 +91,25 @@ public class PersistDataTask extends AsyncTask<Void, Void, Void> {
             }
 
             final float[] valuesAsFloat = valuesQueue.remove();
-            final String[] valuesAsString = new String[3];
-            for (int i = 0; i < valuesAsFloat.length; i++) {
-                valuesAsString[i] = String.valueOf(valuesAsFloat[i]);
-            }
             Log.d(TAG, "persisting data");
-            csvWriter.writeNext(valuesAsString);
+            for (int i = 0; i < fileWriters.size(); i++) {
+                try {
+                    fileWriters.get(i).append(COMMA_DELIMITER);
+                    fileWriters.get(i).append(String.valueOf(valuesAsFloat[i]));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     @Override
     protected void onCancelled() {
         try {
-            fileWriter.close();
+            for (final FileWriter fileWriter : fileWriters) {
+                fileWriter.append(NEW_LINE_SEPARATOR);
+                fileWriter.close();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
